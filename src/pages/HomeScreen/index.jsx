c42c24fd1e233f66b366580aea167e2a31326100
varekaiwork <serhiisaviukwork@gmail.com>
2021-08-10 19:26:58 +0300

    last

--- src/pages/HomeScreen/index.jsx
+++ src/pages/HomeScreen/index.jsx
@@ -9,11 +9,13 @@ import { fetchProducts } from '../../redux/slices/productsSlice';
 import s from './HomeScreen.module.scss';
 
 const HomeScreen = () => {
+	const { activeCategory } = (state) => state.filter;
 	const { productsList: products, limit } = useSelector((state) => state.products);
 	const searchValue = useSelector((state) => state.filter.searchValue);
 	const isProductsExistBySearch = searchValue.length > 0 && products.length === 0;
 	const dispatch = useDispatch();
 	const showLoadMore = products.length > 0 && products.length % limit === 0;
+	const noItemsInCategory = activeCategory !== null && products.length === 0 && searchValue.length === 0;
 	React.useEffect(() => {
 		if (products.length === 0 && searchValue.length === 0) {
 			dispatch(fetchProducts());
@@ -27,9 +29,14 @@ const HomeScreen = () => {
 				<ProductsList products={products} />
 				{showLoadMore && <LoadMore />}
 				{isProductsExistBySearch && (
-					<div className={s.noResultBySearch}>
+					<div className={s.noResult}>
 						<h2>No Results Found</h2>
 						<p>We did not find any article that matches this search Make sure that the search text is entered correctly Try using other search criteria</p>
+					</div>
+				)}
+				{noItemsInCategory && (
+					<div className={s.noResult}>
+						<h2>No items in this category yet</h2>
 					</div>
 				)}
 			</div>
